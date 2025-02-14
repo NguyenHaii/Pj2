27801ae771bc9648d373bdbe4b003722aa40abdc
NguyenHaii <167160632+NguyenHaii@users.noreply.github.com>
2025-02-14 12:14:35 +0700

    Delete Pj2 directory

--- Pj2/src/main/java/quanlynet/validation/UsernameValidator.java
+++
@@ -1,8 +0,0 @@
-package quanlynet.validation;
-
-public class UsernameValidator {
-
-    public static boolean isValidUsername(String username) {
-        return username != null && !username.contains(" ");
-    }
-}
